1ddc7aa05f2911f340e7e8c1c989fe4db1028e27
miryala veronica <veronicamiryala001@gmail.com>
2023-01-17 21:27:39 +0530

    google web pg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import google from './google.png'
+import microphone from './microphone.png'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={google} alt="google" />
+        <div class="container">
+          <input type="text" placeholder="  Search Google or type a URL" />
+          <img src={microphone} alt="microphone" />
+
+        </div>
+        <div className="position">
+        <button id="searcch"> google search</button>
+        <button >i'm feeling lucky</button>
+        </div>
+       
       </header>
+
+      <user />
     </div>
   );
 }
